fdd0eac975036a7cb1920a54b168fd13a14c1ccc
thislily <lilyclairewatson@gmail.com>
2024-09-13 22:28:49 +0200

    omg

--- cypress.config.js
+++ cypress.config.js
@@ -5,6 +5,6 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       // Implement node event listeners here
     },
-    baseUrl: 'http://localhost:8081', // Updated to match your webpack-dev-server
+    baseUrl: 'http://127.0.0.1:8080', // Updated to match your webpack-dev-server
   },
 });
